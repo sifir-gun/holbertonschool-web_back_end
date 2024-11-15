07cb1355a2bb4ee694e9255c6dd659adddcf2b36
sifir-gun <guney.tasdelen74@gmail.com>
2024-11-15 15:55:57 +0100

    delete true0false and if

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,6 +1,7 @@
-export default function taskBlock() {
-  let task = false;
-  let task2 = true;
+export default function taskBlock()
+{
+  const task = false;
+  const task2 = true;
 
   return [task, task2];
 }
