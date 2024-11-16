82b1fd9de705b5a4a0beeaa022330c8f555feb43
sifir-gun <guney.tasdelen74@gmail.com>
2024-11-16 11:53:30 +0100

    task 8

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,15 +1,22 @@
 export default class HolbertonClass {
   constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+
     this._size = size;
     this._location = location;
   }
 
-  // Conversion en Number pour retourner la taille
+  // Casting to Number returns the size
   valueOf() {
     return this._size;
   }
 
-  // Conversion en String pour retourner la localisation
+  // Casting to String returns the location
   toString() {
     return this._location;
   }
