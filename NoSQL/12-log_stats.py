c4a56086d40da94f3d60da320c7bee23d1913b98
sifir-gun <guney.tasdelen74@gmail.com>
2024-12-13 12:58:51 +0100

    Fix indentation for methods in 12-log_stats to match expected output

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,33 +5,25 @@ Script to provide statistics about Nginx logs stored in MongoDB.
 
 from pymongo import MongoClient
 
-
 def log_stats():
     """
     Provides stats about Nginx logs stored in MongoDB.
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collecte = db.nginx
+    nginx_collection = client.logs.nginx
 
-    # Nombre total de logs
-    log_count = collection.count_documents({})
+    total_logs = nginx_collection.count_documents({})
     print(f"{total_logs} logs")
 
-    # Méthodes
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = collection.count_documents({"method": method})
-        # Quatre espaces avant "method"
-        # pour correspondre exactement à l'exemple
-        print(f"\tmethod {method}: {count}")
+        count = nginx_collection.count_documents({"method": method})
+        # Aucune indentation avant 'method'
+        print(f"method {method}: {count}")
 
-    # Compter les requêtes status
-    status_checks = collection.count_documents(
-      {"method": "GET", "path": "/status"})
+    status_checks = nginx_collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_checks} status check")
-
 
 if __name__ == "__main__":
     log_stats()
