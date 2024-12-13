e6b44e40c69e7dffb29008da6a3a810b1d40ff29
sifir-gun <guney.tasdelen74@gmail.com>
2024-12-13 11:38:36 +0100

    add blank lines

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,6 +5,7 @@ Script to provide statistics about Nginx logs stored in MongoDB.
 
 from pymongo import MongoClient
 
+
 def log_stats():
     """
     Provides stats about Nginx logs stored in MongoDB.
@@ -21,12 +22,15 @@ def log_stats():
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        # Quatre espaces avant "method" pour correspondre exactement à l'exemple
+        # Quatre espaces avant "method"
+        # pour correspondre exactement à l'exemple
         print("    method {}: {}".format(method, count))
 
     # Compter les requêtes status
-    status_checks = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    status_checks = nginx_collection.count_documents(
+      {"method": "GET", "path": "/status"})
     print(f"{status_checks} status check")
+
 
 if __name__ == "__main__":
     log_stats()
