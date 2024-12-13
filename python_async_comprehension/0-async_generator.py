bee5fcd59dfaf704bdb1dc079b9b0bc170a5964a
sifir-gun <guney.tasdelen74@gmail.com>
2024-12-13 12:05:45 +0100

    delete async

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@ entre chaque génération.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine asynchrone qui génère 10 nombres aléatoires compris
     entre 0 et 10.
