ed8631e1ac9fdd40211bbd2db85aabf48ca8f01c
sifir-gun <guney.tasdelen74@gmail.com>
2025-01-23 11:14:48 +0100

    delete /n

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,18 +6,13 @@ const rl = readline.createInterface({
 });
 
 // Prompt the user for their name
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+rl.question('Welcome to Holberton School, what is your name? ', (name) => {
   console.log(`Your name is: ${name}`);
-  // If running in interactive mode, the input stream will remain open until the user ends it.
-  // For piped input, the stream will close immediately after the data is consumed.
   rl.close();
 });
 
 // When the input stream ends, display the closing message.
 rl.on('close', () => {
-  // Only display the closing message if input is being piped.
-  // In interactive mode the user might not end the input (with ctrl+D), so the closing message
-  // will only appear when the stream is closed.
   if (!process.stdin.isTTY) {
     console.log('This important software is now closing');
   }
