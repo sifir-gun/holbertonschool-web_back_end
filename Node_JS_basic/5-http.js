3009f39ca30d921e790441d92a6d6b04c0488d04
sifir-gun <guney.tasdelen74@gmail.com>
2025-01-23 15:01:56 +0100

    add ctch

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,30 +1,21 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const database = process.argv[2];
-
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+
   if (req.url === '/') {
-    // Si l'URL est "/", on renvoie directement le message d'accueil.
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    // Si l'URL est "/students", on commence par envoyer la première ligne.
-    const output = 'This is the list of our students\n';
-    // On appelle la fonction countStudents qui lit le fichier CSV de manière asynchrone.
-    countStudents(database)
-      .then((text) => {
-        // Une fois la lecture terminée, on concatène le résultat au message d'en-tête.
-        res.end(output + text);
-      })
-      .catch((error) => {
-        // En cas d'erreur (fichier non trouvé, etc.),
-		// on renvoie le message d'erreur après l'en-tête.
-        res.end(output + error.message);
-      });
-  } else {
-    // Pour toutes les autres URL, on renvoie par défaut le message d'accueil.
-    res.end('Hello Holberton School!');
+    try {
+      res.write('This is the list of our students\n');
+      await countStudents(process.argv[2])
+        .then((output) => {
+          res.end(output);
+        });
+    } catch (error) {
+      res.end(`This is the list of our students\n${error.message}`);
+    }
   }
 });
 
