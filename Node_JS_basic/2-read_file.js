663497ff8ab7b04d73c1d03e4604e53ca44f86e4
sifir-gun <guney.tasdelen74@gmail.com>
2025-01-23 11:48:32 +0100

    add task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -16,8 +16,10 @@ function countStudents(path) {
     });
 
     for (const field in fields) {
-      const list = fields[field];
-      console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        const list = fields[field];
+        console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+      }
     }
   } catch (error) {
     throw new Error('Cannot load the database');
