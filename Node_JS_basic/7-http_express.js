e6502c8450076b9713ed0388664263f7c0399854
sifir-gun <guney.tasdelen74@gmail.com>
2025-01-23 15:24:23 +0100

    feat: create Express server with student data endpoint

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,25 +2,21 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-const database = process.argv[2];
+const port = 1245;
 
 app.get('/', (req, res) => {
-  res.type('text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  res.type('text/plain');
-  const output = 'This is the list of our students\n';
-  countStudents(database)
-    .then((text) => {
-      res.send(output + text);
-    })
-    .catch((error) => {
-      res.send(output + error.message);
-    });
+app.get('/students', async (req, res) => {
+  try {
+    const output = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${output}`);
+  } catch (error) {
+    res.send(`This is the list of our students\n${error.message}`);
+  }
 });
 
-app.listen(1245);
+app.listen(port);
 
 module.exports = app;
