f5c8fbe5bb80a8fbbb3eafcd006f2b5581fc08d4
sifir-gun <guney.tasdelen74@gmail.com>
2025-01-23 11:38:43 +0100

    process

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,24 +1,14 @@
-// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const readline = require('readline');
+process.stdin.setEncoding('utf8');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-// Affiche le message de bienvenue avec un saut de ligne
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  // Affiche le nom de l'utilisateur avec un saut de ligne
-  process.stdout.write(`Your name is: ${name}\n`);
-  rl.close();
-});
-
-// Lors de la fermeture de l'interface readline
-rl.on('close', () => {
-  // Vérifie si l'entrée n'est pas en mode TTY (piped)
-  if (!process.stdin.isTTY) {
-    // Affiche le message de fermeture avec un saut de ligne
-    process.stdout.write('This important software is now closing\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
