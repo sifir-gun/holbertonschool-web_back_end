9fadfacf711a9452096e559af0b0510fbf29c06d
sifir-gun <guney.tasdelen74@gmail.com>
2025-01-23 14:28:00 +0100

    add () for line 7 and 12

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,12 +4,12 @@ function countStudents(path) {
   try {
     const fileContent = fs.readFileSync(path, 'utf8');
     const lines = fileContent.trim().split('\n');
-    const students = lines.slice(1).filter(line => line.length > 0);
+    const students = lines.slice(1).filter((line) => line.length > 0);
 
-    console.log(`Number of students: ${students.length}`);
+    let output = `Number of students: ${students.length}\n`;
 
     const fields = {};
-    students.forEach(student => {
+    students.forEach((student) => {
       const [firstname, , , field] = student.split(',');
       if (!fields[field]) fields[field] = [];
       fields[field].push(firstname);
@@ -18,9 +18,12 @@ function countStudents(path) {
     for (const field in fields) {
       if (Object.prototype.hasOwnProperty.call(fields, field)) {
         const list = fields[field];
-        console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+        output += `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}\n`;
       }
     }
+
+    console.log(output.trim());
+    return output;
   } catch (error) {
     throw new Error('Cannot load the database');
   }
